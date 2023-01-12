16fb4d97e14d7e65347500ef92428279f1a602af
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2023-01-12 15:31:48 +0530

    docs: custom OpenAPI override for documentation

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ def ping():
 async def validate(data: str):
     result = await check_plan_valid(data)
     return result
+
+# For defining custom documentation for the server
+def SubstraitFiddleOpenAPI():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Substrait Fiddle",
+        version="0.0.1",
+        description="Documentation for Substrait Fiddle APIs",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    print(openapi_schema)
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = SubstraitFiddleOpenAPI
