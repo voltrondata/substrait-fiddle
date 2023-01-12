4b2ccfef8ad1b132d8e4cdc940e7817585022bea
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2023-01-12 15:31:11 +0530

    feat: Validate API using substrait-validator

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.openapi.utils import get_openapi
 
+from substrait_validator import check_plan_valid
 
 app = FastAPI()
 
 @app.get("/")
 def ping():
     return {"api_service": "up and running"}
+
+@app.post("/validate/")
+async def validate(data: str):
+    result = await check_plan_valid(data)
+    return result
