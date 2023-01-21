a20805077415a73cd49832c26e6f94fd77027d53
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2023-01-22 02:29:48 +0530

    feat: DuckDB Initialization and SQL to Substrait Parsing

--- app.py
+++ app.py
@@ -1,18 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.openapi.utils import get_openapi
 
 from substrait_validator import check_plan_valid
+import duckdb
 
 app = FastAPI()
+con = None
+
 
 @app.get("/")
 def ping():
     return {"api_service": "up and running"}
 
+
+@app.on_event("startup")
+def Initialize():
+    global con
+    con = duckdb.connect()
+    con.install_extension("substrait")
+    con.load_extension("substrait")
+    print("DuckDb initialized successfully")
+
+
 @app.post("/validate/")
-async def validate(data: str):
-    result = await check_plan_valid(data)
-    return result
+async def Validate(data: dict):
+    try:
+        result = await check_plan_valid(data)
+        return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail="Substrait Validator Internal Error: " + str(e)
+        )
+
+
+@app.post("/init/duckdb/", status_code=status.HTTP_200_OK)
+async def InjectDuckDb(data: list[str]):
+    try:
+        global con
+        for i in data:
+            con.execute(query=i)
+        return {"message": "DuckDB Operation successful"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="Substrait DuckDB Internal Error while parsing SQL Query: " + str(e),
+        )
+
+
+@app.post("/parse/")
+async def ParseToSubstrait(data: str):
+    try:
+        global con
+        result = con.get_substrait_json(data).fetchone()[0]
+        print(result)
+        return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="Substrait DuckDB Internal Error while parsing SQL Query: " + str(e),
+        )
+
 
 # For defining custom documentation for the server
 def SubstraitFiddleOpenAPI():
@@ -27,8 +74,8 @@ def SubstraitFiddleOpenAPI():
     openapi_schema["info"]["x-logo"] = {
         "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
     }
-    print(openapi_schema)
     app.openapi_schema = openapi_schema
     return app.openapi_schema
+
 
 app.openapi = SubstraitFiddleOpenAPI
