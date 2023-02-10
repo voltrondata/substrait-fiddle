0745f19542ba58d52a955b24f065f871fe5c70e3
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2023-02-10 14:14:42 +0530

    cleanup: remove directive

--- src/assets/js/directive.js
+++
@@ -1,7 +0,0 @@
-export const icon = {
-  mounted(el, binding, vnode) {
-    el.innerHTML = `<svg class="bi" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
-    <use xlink:href="#${binding.value}"/>
-  </svg>`
-  }
-};
--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ import router from "./router";
 import "./assets/css/main.css";
 import "./assets/css/styles.scss";
 
-import { icon } from './assets/js/directive';
 
 const app = createApp(App);
-
-app.directive('icon', icon);
 
 app.use(router);
 
